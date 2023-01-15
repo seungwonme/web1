713116c330538854f0eb80ab54676288ee056b4c
seungwonme <seungwonan.me@gmail.com>
2023-01-15 23:33:59 +0900

    add class dark

--- js/app.js
+++ js/app.js
@@ -4,25 +4,19 @@ const web = document.getElementById('webTab');
 const html = document.getElementById('htmlTab');
 const css = document.getElementById('cssTab');
 const js = document.getElementById('jsTab');
-const links = document.querySelectorAll('a')
+const links = document.querySelectorAll('a');
 
 function nightDayHandler() {
     if (button.value === 'night') {
-        body.style.background = '#333';
-        body.style.color = 'white';
-        for (const link of links) {
-            link.style.color = 'pink'
-        }
+        body.classList.add('dark');
         button.value = 'day';
     } else if (button.value === 'day') {
-        body.style.background = 'white';
-        body.style.color = '#333';
-        for (const link of links) {
-            link.style.color = '#333'
-        }
+        body.classList.remove('dark');
         button.value = 'night';
     }
 }
+
+// tabview
 button.addEventListener('click', nightDayHandler);
 web.addEventListener('click', function () {
     document.getElementById('web').classList.add('on');
